354cc4f573c2a4b65c45d35f7b06284b99ef1ead
Carlos Fierro <carlos.afv95@gmail.com>
2026-08-04 20:04:23 -0600

    Implemented TODOs in threading exammples

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -8,12 +8,26 @@
 //#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
 #define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
 
+#define MILISECONDS_TO_MICROSECONDS 1000
+
 void* threadfunc(void* thread_param)
 {
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    if(thread_param == NULL)
+    {
+        return NULL;
+    }
+    
+    struct thread_data* data = (struct thread_data *)thread_param;
+    usleep(data->wait_ms * MILISECONDS_TO_MICROSECONDS);
+    pthread_mutex_lock(data->mutex);
+    usleep(data->wait_to_release_ms * MILISECONDS_TO_MICROSECONDS);
+    data->thread_complete_success = true;
+    pthread_mutex_unlock(data->mutex);
+
     return thread_param;
 }
 
@@ -28,6 +42,24 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    struct thread_data* data = (struct thread_data *)malloc(sizeof(struct thread_data));
+    if (data == NULL)
+    {
+        return false;
+    }
+
+    // populating the data struct
+    data->wait_ms = wait_to_obtain_ms;
+    data->wait_to_release_ms = wait_to_release_ms;
+    data->thread_complete_success = false;
+    data->mutex = mutex;
+
+    int rc = pthread_create(thread, NULL, threadfunc, data);
+    if (rc != 0)
+    {
+        return false;
+    }
+    
+    return true;
 }
 
